ef49a413f98473edf1fc9672afee07a3848058c7
hackerfeast <98017419+hackerfeast1@users.noreply.github.com>
2022-01-31 13:37:31 +0530

    Update automate_emails.py

--- automate_emails.py
+++ automate_emails.py
@@ -5,9 +5,9 @@ email_list = ("****@email.com", "****@gmail.com")
 credentials = {"email": "Your email id here", "pwd": "password"}
 
 for email in email_list:
-  s = smtplib.SMTP("smtp.gmail.com", 587)
-  s.starttls()
-  s.login(credentials["email"], credentials['pwd'])
-  message = "Your message"
-  s.sendmail(credentials['email'], email, message)
+	s = smtplib.SMTP("smtp.gmail.com", 587)
+ 	s.starttls()
+  	s.login(credentials["email"], credentials['pwd'])
+  	message = "Your message"
+  	s.sendmail(credentials['email'], email, message)
 	s.quit()
